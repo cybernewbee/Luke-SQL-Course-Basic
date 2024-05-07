6c2e379057544c1bc31a221fbc8eab8dff402e37
cybernewbee <121833641+cybernewbee@users.noreply.github.com>
2024-05-07 12:16:06 +0800

    Update and rename Problem.sql to Luke Practice Problem 5.sql

---
+++ Luke Practice Problem 5.sql
@@ -0,0 +1,12 @@
+DB:https://lukeb.co/sql_jobs_db
+-- prompt: find the average salary and number of job postings for each skill --
+-- hint: combine job_postings, skill_dim, and skill_job_dim
+SELECT 
+	skills.skills AS skill_name, 
+    	COUNT (job_postings.job_title) AS numbers_of_job_postings,  
+    	AVG (job_postings.salary_year_avg) AS avg_salary_for_skills 
+FROM 
+	job_postings_fact AS job_postings
+INNER JOIN
+	skills_job_dim AS skills_to_job ON job_postings.job_id = skills_to_job.job_id
+INNER JOIN
--- Problem.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	skills.skills AS skill_name, 
-    COUNT (job_postings.job_title) AS numbers_of_job_postings,  
-    AVG (job_postings.salary_year_avg) AS avg_salary_for_skills 
-FROM 
-	job_postings_fact AS job_postings
-INNER JOIN
-	skills_job_dim AS skills_to_job ON job_postings.job_id = skills_to_job.job_id
-INNER JOIN
